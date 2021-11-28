4129c543f8213f1d49fe4df8fbac637cf9649e3b
João Vieira <joaovieira@morgana.joaomiguelvieira.com>
2021-11-28 18:53:55 +0000

    Finished Minotaur v1.1

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # Minotaur
+Minotaur is an Arduino-based control board for automatic swing gates. This repository contains a full-stack implementation of Minotaur, including the fabrication files of the PCB, the list of parts, and the firmware to be loaded into the Arduino microcontroller.
+
+![minotaur_installation](img/minotaur_installation.png)
+
+- [The story behind Minotaur](the-story-behind-minotaur)
+- [Disclaimer](disclaimer)
+- [Content of this repository](content-of-this-repository)
+- [Hardware requirements](hardware-requirements)
+- [Getting started](getting-started)
+- [Using Minotaur](using-minotaur)
+  - [Programming mode](programming-mode)
+  - [Normal mode](normal-mode)
+- [Final notes](final-notes)
+
+## The story behind Minotaur
+A long time ago, my father spent over 500 € to install an automatic swing gate system on his property. The system was operational for over ten years, but recently I got a puppy whose favorite hobby was to destroy pretty much everything he could get his paws on (including the gate system). Since the gate system was already old and outdated, I struggled to find a control board that was compatible with the existing hardware. On top of that, the boards I found were too expensive, and the engineering was doubtful.
+
+Therefore, I have decided to design myself a control board from scratch that could operate the hardware that my dog was merciful enough to spare. After a few months of procrastination and another few of development and debugging, Minotaur was born!
+
+## Disclaimer
+* **Minotaur only includes the control board**. The remaining hardware has to be purchased separately.
+* All external hardware connected to Minotaur is powered with 12 V or -12 V, namely the arms, the lamp, and the ID control module.
+* **Do plug Minotaur into your own system if you are not sure of its requirements!**
+
+## Content of this repository
+* `/doc`: contains documentation regarding minotaur, namely the electronic schematics of the PCB;
+* `/img`: contains images of the PCB and the illustration of a typical installation;
+* `/pcb`: contains the files required to fabricate the PCB (Gerber) as well as the Bill of Materials (BOM), and a full backup of the EasyEDA project;
+* `/src`: contains the source code of Minotaur that will run in the Arduino Nano installed in the PCB.
+
+## Hardware requirements
+The following hardware is required to build a system using Minotaur:
+
+* An ATX power supply **(500 W minimum)**;
+* An ID control module (connected to an antenna) **powered with 12 V** that acts as a button: when the antenna receives a valid signal (sent by a remote controller), it acts as a pressed button, short-circuiting the input and the output;
+* Two **+12 V/-12 V powered** arms: with +12 V, the arms open; with -12 V, the arms close;
+* An Arduino Nano.
+
+## Getting started
+
+PCB Front |  PCB Back
+-|-
+![pcb_front](/img/minotaur_v1_1_pcb_front.svg) | ![pcb_back](/img/minotaur_v1_1_pcb_back.svg)
+
+* Get the Minotaur PCB (using the [Gerber file](/pcb/minotaur_v1_1_pcb_gerber.zip)) and the parts (listed in the [BOM](/pcb/minotaur_v1_1_BOM.csv)).
+* Solder all components in the PCB according to the [schematics](doc/minotaur_v1_1_pcb_schematics.pdf). Additionally, solder two headers where the Arduino Nano will fit so that it can be removed or replaced.
+* Plug the Arduino Nano into PCB and program it.
+* To make sure that everything is working as it should, I find it helpful to connect the inputs and outputs of the board to dummy components (LEDs where the arms should be, a button instead of the antenna).
+* Mount Minotaur as shown in [the example](/img/minotaur_installation.png).
+* It is now time to **enter the Programming mode** of Minotaur and get your setup working.
+
+## Using Minotaur
+Minotaur has two operation modes: **Programming** and **Normal**. By default, Minotaur is always operating in the Normal mode.
+
+### Programming mode
+The Programming mode serves to configure Minotaur with the opening and closing times of your swing gate. **The programming mode has to be executed right after installing the system to configure the opening and closing times of your swing gate. The gate must be fully closed before entering Programming mode.**
+
+To enter the Programming mode, press the `PROG` button. You will hear three beeps and the lamp will start blinking. Press the `SELECT` button or activate the remote to start opening the left arm. Then, press it again to start opening the right arm. Press again `SELECT` when the left arm is fully opened, and again when the right arm finishes opening. Then, press `SELECT` to start closing the right arm and again to start closing the left arm. Press it again when the right arm is fully closed and again when the left arm finishes closing. You will hear two beeps and the lamp will stop blinking. Minotaur will now enter the Normal mode.
+
+During the Programming cycle, Minotaur will learn the opening and closing times of your gate. If everything went well, you should now be able to open and close your gate by pressing `SELECT` or activating the remote. If that is not the case, feel free to execute another Programming cycle.
+
+### Normal mode
+* Press `SELECT` or activate the remote to open or close your swing gate.
+* Press and hold `CLOSE` to close the left arm.
+* Press and hold `CLOSE` and `FUNC` simultaneously to close the right arm.
+* Press and hold `OPEN` to open the left arm.
+* Press and hold `OPEN` and `FUNC` simultaneously to open the right arm.
+* Press `PROG` to enter the Programming mode.
+
+## Final notes
+I decided to make Minotaur source available for multiple reasons:
+* First, since I did not find related projects online, I hope someone else may find it interesting and useful. Also, since every component of Minotaur is open-source, it can be easily modified and adapted to the needs of other people.
+* Second, I found it an interesting proof o concept to show that I master the technologies used in the development of Minotaur :smile:
+
+Feel free to make suggestions on how to improve Minotaur!
---
+++ src/v1_1/minotaur/minotaur.ino
@@ -0,0 +1,366 @@
+/* 
+ * Title: Minotaur v1.1 firmware
+ * Author: Joao Vieira
+ * Date: November 28th, 2021
+ * 
+ * Description: This is the firmware for the Minotaur board for
+ * controlling swing automatic gates. The Minotaur board does not
+ * include any ID decoder. That is an extra piece of hardware 
+ * that should be connected to the board acting like a button
+ * (closed when activated and open when deactivated).
+ */
+
+// libraries
+#include <Arduino_FreeRTOS.h>
+#include <EEPROM.h>
+
+// define if debug
+#define DEBUG
+
+// inputs and outputs
+#define VCC_ARM_SW    3  // OUTPUT switch arm voltage
+#define LEFT_ARM_PIN  4  // OUTPUT activate left arm
+#define RIGHT_ARM_PIN 5  // OUTPUT activate right arm
+#define LAMP_PIN      6  // OUTPUT activate lamp
+#define BUZ_PIN       7  // OUTPUT activate buzzer
+#define BTN_CLOSE     8  // INPUT  button close selected
+#define BTN_PROGRAM   9  // INPUT  button program selected
+#define BTN_SELECT    10 // INPUT  button select selected
+#define BTN_FN        11 // INPUT  button function selected
+#define BTN_OPEN      12 // INPUT  button open selected
+
+// macros for logical values
+#define ARM_OFF   LOW
+#define ARM_ON    HIGH
+#define ARM_CLOSE LOW
+#define ARM_OPEN  HIGH
+#define LAMP_OFF  LOW
+#define LAMP_ON   HIGH
+#define BTN_ON    HIGH
+#define BTN_OFF   LOW
+
+// other macros
+#define BUZZER_FREQ  1000 // buzzer frequency [Hz]
+#define BUZZER_BIP_T 1000 // buzzer bip period [ms]
+#define LAMP_BLINK_T 1000 // lamp blink period [ms]
+#define OPEN_CLOSE_DELAY LAMP_BLINK_T * 2 // time between lamp starts blinking and gate starts moving
+#define NUMBER_BIPS_BEFORE_PROG 3 // number of bips before entering programming mode
+#define NUMBER_BIPS_AFTER_PROG 2 // number of bips after exiting programming mode
+#define PROG_BTN_DELAY 500 // delay after pressing button in programming mode [ms]
+
+// buttons
+#define N_DIGITAL_PINS 14 // size of the debounce array
+#define DEBOUNCE_DELAY 50 // miliseconds for debounce delay
+
+volatile bool btnState[N_DIGITAL_PINS];          // state of buttons is read from here
+bool lastBtnState[N_DIGITAL_PINS];               // auxiliary debounce variable
+unsigned long lastDebounceTime[N_DIGITAL_PINS];  // auxiliary debounce variable
+
+// state variables
+volatile bool lampOn;
+bool gateOpen;
+unsigned long leftArmOpenTime, leftArmCloseTime;
+unsigned long rightArmOpenTime, rightArmCloseTime;
+unsigned long armsOpenDelta, armsCloseDelta;
+
+// eeprom addresses
+#define EEPROM_ULONG_SIZE                4 // size of unsigned long
+#define EEPROM_LEFT_ARM_OPEN_TIME_ADDR   0
+#define EEPROM_LEFT_ARM_CLOSE_TIME_ADDR  EEPROM_LEFT_ARM_OPEN_TIME_ADDR   + EEPROM_ULONG_SIZE
+#define EEPROM_RIGHT_ARM_OPEN_TIME_ADDR  EEPROM_LEFT_ARM_CLOSE_TIME_ADDR  + EEPROM_ULONG_SIZE
+#define EEPROM_RIGHT_ARM_CLOSE_TIME_ADDR EEPROM_RIGHT_ARM_OPEN_TIME_ADDR  + EEPROM_ULONG_SIZE
+#define EEPROM_ARMS_OPEN_DELTA_ADDR      EEPROM_RIGHT_ARM_CLOSE_TIME_ADDR + EEPROM_ULONG_SIZE
+#define EEPROM_ARMS_CLOSE_DELTA_ADDR     EEPROM_ARMS_OPEN_DELTA_ADDR      + EEPROM_ULONG_SIZE
+
+/*
+ * ===================================================================
+ * ==================== ARDUINO BULT-IN FUNCTIONS ====================
+ * ===================================================================
+ */
+
+void setup() {
+  // setup inputs and outputs
+  pinMode(VCC_ARM_SW,    OUTPUT);
+  pinMode(LEFT_ARM_PIN,  OUTPUT);
+  pinMode(RIGHT_ARM_PIN, OUTPUT);
+  pinMode(LAMP_PIN,      OUTPUT);
+  pinMode(BUZ_PIN,       OUTPUT);
+  pinMode(BTN_CLOSE,     INPUT);
+  pinMode(BTN_PROGRAM,   INPUT);
+  pinMode(BTN_SELECT,    INPUT);
+  pinMode(BTN_FN,     INPUT);
+  pinMode(BTN_OPEN,      INPUT);
+
+  // activate buzzer until setup completes
+  tone(BUZ_PIN, BUZZER_FREQ);
+
+#ifdef DEBUG
+  // open serial console
+  Serial.begin(9600);
+  while (!Serial); // wait for serial to connect
+#endif
+
+  // initialize outputs
+  digitalWrite(LEFT_ARM_PIN, ARM_OFF);
+  digitalWrite(RIGHT_ARM_PIN, ARM_OFF);
+  digitalWrite(LAMP_PIN, LAMP_OFF);
+
+  // initialize global variables
+  for (int i = 0; i < N_DIGITAL_PINS; i++) {
+    btnState[i] = BTN_OFF;
+    lastBtnState[i] = BTN_OFF;
+    lastDebounceTime[i] = 0;
+  }
+
+  lampOn = false;
+
+  // load open and close timings from persistent memory
+  EEPROM.get(EEPROM_LEFT_ARM_OPEN_TIME_ADDR,   leftArmOpenTime);
+  EEPROM.get(EEPROM_LEFT_ARM_CLOSE_TIME_ADDR,  leftArmCloseTime);
+  EEPROM.get(EEPROM_RIGHT_ARM_OPEN_TIME_ADDR,  rightArmOpenTime);
+  EEPROM.get(EEPROM_RIGHT_ARM_CLOSE_TIME_ADDR, rightArmCloseTime);
+  EEPROM.get(EEPROM_ARMS_OPEN_DELTA_ADDR,      armsOpenDelta);
+  EEPROM.get(EEPROM_ARMS_CLOSE_DELTA_ADDR,     armsCloseDelta);
+
+  // launch threads
+  xTaskCreate(taskDebounceBtns, "Debounce buttons", 128, NULL, 3, NULL);
+  xTaskCreate(taskBlinkLamp,    "Blink lamp",       128, NULL, 3, NULL);
+  xTaskCreate(taskMain,         "Main routine",     128, NULL, 3, NULL);
+
+  // deactivate buzzer when setup completes
+  noTone(BUZ_PIN);
+}
+
+void loop() {
+  // workload is dealt with in tasks
+}
+
+/*
+ * ===================================================================
+ * ======================== AUXILIARY ROUTINES =======================
+ * ===================================================================
+ */
+
+void debounceBtn(int btn) {
+  int reading = digitalRead(btn);
+
+  if (reading != lastBtnState[btn]) {
+    lastDebounceTime[btn] = millis();
+  }
+
+  if ((millis() - lastDebounceTime[btn]) > DEBOUNCE_DELAY) {
+    if (reading != btnState[btn]) {
+      btnState[btn] = reading;
+    }
+  }
+
+  lastBtnState[btn] = reading;
+}
+
+void bipBuzzer(int bips) {
+  for (int i = 0; i < bips; i++) {
+    tone(BUZ_PIN, BUZZER_FREQ);
+    vTaskDelay(BUZZER_BIP_T / 2 / portTICK_PERIOD_MS);
+    noTone(BUZ_PIN);
+    vTaskDelay(BUZZER_BIP_T / 2 / portTICK_PERIOD_MS);
+  }
+}
+
+void openCloseCycle() {
+  // switch arm voltage depending on gate state
+  digitalWrite(VCC_ARM_SW, gateOpen ? ARM_CLOSE : ARM_OPEN);
+  
+  // start blinking lamp
+  lampOn = true;
+  vTaskDelay(OPEN_CLOSE_DELAY / portTICK_PERIOD_MS);
+
+  // start first arm
+  digitalWrite(gateOpen ? RIGHT_ARM_PIN : LEFT_ARM_PIN, ARM_ON);
+  vTaskDelay((gateOpen ? armsCloseDelta : armsOpenDelta) / portTICK_PERIOD_MS);
+
+  // start second arm
+  digitalWrite(gateOpen ? LEFT_ARM_PIN : RIGHT_ARM_PIN, ARM_ON);
+  vTaskDelay((gateOpen ? (rightArmCloseTime - armsCloseDelta) : (leftArmOpenTime - armsOpenDelta)) / portTICK_PERIOD_MS);
+
+  // stop first arm
+  digitalWrite(gateOpen ? RIGHT_ARM_PIN : LEFT_ARM_PIN, ARM_OFF);
+  vTaskDelay((gateOpen ? (leftArmCloseTime - rightArmCloseTime + armsCloseDelta) : (rightArmOpenTime - leftArmOpenTime + armsOpenDelta)) / portTICK_PERIOD_MS);
+
+  // stop second arm
+  digitalWrite(gateOpen ? LEFT_ARM_PIN : RIGHT_ARM_PIN, ARM_OFF);
+
+  // stop blinking lamp
+  lampOn = false;
+
+  // switch gate state
+  gateOpen = !gateOpen;
+}
+
+void closeWhilePressed() {
+  // set voltage to close mode
+  digitalWrite(VCC_ARM_SW, ARM_CLOSE);
+
+  // continue closing until button not pressed
+  while (btnState[BTN_CLOSE] == BTN_ON) {
+    lampOn = true;
+    digitalWrite(btnState[BTN_FN] == BTN_ON ? RIGHT_ARM_PIN : LEFT_ARM_PIN, ARM_ON);
+  }
+
+  // stop both arms from moving
+  digitalWrite(LEFT_ARM_PIN, ARM_OFF);
+  digitalWrite(RIGHT_ARM_PIN, ARM_OFF);
+  lampOn = false;
+}
+
+void openWhilePressed() {
+  // set voltage to open mode
+  digitalWrite(VCC_ARM_SW, ARM_OPEN);
+
+  // continue opening until button not pressed
+  while (btnState[BTN_OPEN] == BTN_ON) {
+    lampOn = true;
+    digitalWrite(btnState[BTN_FN] == BTN_ON ? RIGHT_ARM_PIN : LEFT_ARM_PIN, ARM_ON);
+  }
+
+  // stop both arms from moving and lamp from blinking
+  digitalWrite(LEFT_ARM_PIN, ARM_OFF);
+  digitalWrite(RIGHT_ARM_PIN, ARM_OFF);
+  lampOn = false;
+}
+
+void programCycle() {
+  // the gate must be fully closed when entering programming mode
+  
+  // inform user that entered programming mode
+  bipBuzzer(NUMBER_BIPS_BEFORE_PROG);
+  
+  lampOn = true; // turn lamp on
+
+  // set voltage to open
+  digitalWrite(VCC_ARM_SW, ARM_OPEN);
+
+  // open left arm
+  while (btnState[BTN_SELECT] != BTN_ON); // wait for btn
+  digitalWrite(LEFT_ARM_PIN, ARM_ON);
+  unsigned long leftArmStartOpen = millis();
+  vTaskDelay(PROG_BTN_DELAY / portTICK_PERIOD_MS);
+
+  // open right arm
+  while (btnState[BTN_SELECT] != BTN_ON); // wait for btn
+  digitalWrite(RIGHT_ARM_PIN, ARM_ON);
+  unsigned long rightArmStartOpen = millis();
+  vTaskDelay(PROG_BTN_DELAY / portTICK_PERIOD_MS);
+
+  // stop left arm
+  while (btnState[BTN_SELECT] != BTN_ON); // wait for btn
+  digitalWrite(LEFT_ARM_PIN, ARM_OFF);
+  unsigned long leftArmStopOpen = millis();
+  vTaskDelay(PROG_BTN_DELAY / portTICK_PERIOD_MS);
+
+  // stop right arm
+  while (btnState[BTN_SELECT] != BTN_ON); // wait for btn
+  digitalWrite(RIGHT_ARM_PIN, ARM_OFF);
+  unsigned long rightArmStopOpen = millis();
+  vTaskDelay(PROG_BTN_DELAY / portTICK_PERIOD_MS);
+
+  // set voltage to close
+  digitalWrite(VCC_ARM_SW, ARM_CLOSE);
+
+  // close right arm
+  while (btnState[BTN_SELECT] != BTN_ON); // wait for btn
+  digitalWrite(RIGHT_ARM_PIN, ARM_ON);
+  unsigned long rightArmStartClose = millis();
+  vTaskDelay(PROG_BTN_DELAY / portTICK_PERIOD_MS);
+
+  // close left arm
+  while (btnState[BTN_SELECT] != BTN_ON); // wait for btn
+  digitalWrite(LEFT_ARM_PIN, ARM_ON);
+  unsigned long leftArmStartClose = millis();
+  vTaskDelay(PROG_BTN_DELAY / portTICK_PERIOD_MS);
+
+  // stop right arm
+  while (btnState[BTN_SELECT] != BTN_ON); // wait for btn
+  digitalWrite(RIGHT_ARM_PIN, ARM_OFF);
+  unsigned long rightArmStopClose = millis();
+  vTaskDelay(PROG_BTN_DELAY / portTICK_PERIOD_MS);
+
+  // stop left arm
+  while (btnState[BTN_SELECT] != BTN_ON); // wait for btn
+  digitalWrite(LEFT_ARM_PIN, ARM_OFF);
+  unsigned long leftArmStopClose = millis();
+
+  lampOn = false; // turn lamp off
+
+  // recalculate open and close timings
+  leftArmOpenTime = leftArmStopOpen - leftArmStartOpen;
+  leftArmCloseTime = leftArmStopClose - leftArmStartClose;
+  rightArmOpenTime = rightArmStopOpen - rightArmStartOpen;
+  rightArmCloseTime = rightArmStopClose - rightArmStartClose;
+  armsOpenDelta = rightArmStartOpen - leftArmStartOpen;
+  armsCloseDelta = leftArmStartClose - rightArmStartClose;
+
+  // store open and close timings to persistent memory
+  EEPROM.put(EEPROM_LEFT_ARM_OPEN_TIME_ADDR,   leftArmOpenTime);
+  EEPROM.put(EEPROM_LEFT_ARM_CLOSE_TIME_ADDR,  leftArmCloseTime);
+  EEPROM.put(EEPROM_RIGHT_ARM_OPEN_TIME_ADDR,  rightArmOpenTime);
+  EEPROM.put(EEPROM_RIGHT_ARM_CLOSE_TIME_ADDR, rightArmCloseTime);
+  EEPROM.put(EEPROM_ARMS_OPEN_DELTA_ADDR,      armsOpenDelta);
+  EEPROM.put(EEPROM_ARMS_CLOSE_DELTA_ADDR,     armsCloseDelta);
+
+  // inform user that exited programming mode
+  bipBuzzer(NUMBER_BIPS_AFTER_PROG);
+}
+
+/*
+ * ===================================================================
+ * =================== ROUTINES EXECUTED BY THREADS ==================
+ * ===================================================================
+ */
+
+void taskDebounceBtns(void *pvParameters) {
+  (void) pvParameters;
+
+  while (true) { // execute thread forever
+    debounceBtn(BTN_CLOSE);
+    debounceBtn(BTN_PROGRAM);
+    debounceBtn(BTN_SELECT);
+    debounceBtn(BTN_FN);
+    debounceBtn(BTN_OPEN);
+  }
+}
+
+void taskBlinkLamp(void *pvParameters) {
+  (void) pvParameters;
+
+  while (true) { // execute thread forever    
+    if (lampOn) {
+      digitalWrite(LAMP_PIN, LAMP_ON);
+      vTaskDelay(LAMP_BLINK_T / 2 / portTICK_PERIOD_MS);
+      digitalWrite(LAMP_PIN, LAMP_OFF);
+      vTaskDelay(LAMP_BLINK_T / 2 / portTICK_PERIOD_MS);
+    }
+  }
+}
+
+void taskMain(void *pvParameters) {
+  (void) pvParameters;
+
+  while (true) { // execute thread forever
+    // open or close cycle
+    if (btnState[BTN_SELECT] == BTN_ON) {
+      openCloseCycle();
+    }
+    // close while pressed
+    else if (btnState[BTN_CLOSE] == BTN_ON) {
+      closeWhilePressed();
+    }
+    // open while pressed
+    else if (btnState[BTN_OPEN] == BTN_ON) {
+      openWhilePressed();
+    }
+    // program cycle
+    else if (btnState[BTN_PROGRAM] == BTN_ON) {
+      programCycle();
+    }
+  }
+}
